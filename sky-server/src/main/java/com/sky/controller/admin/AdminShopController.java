3e95228bfc824509adbb6a0997baf2195e126c89
“saturnhalo” <970285047@qq.com>
2025-06-09 16:56:39 +0800

    增加功能：店铺经营状态的设置与获取（用户端、管理端）

---
+++ sky-server/src/main/java/com/sky/controller/admin/AdminShopController.java
@@ -0,0 +1,42 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 店铺状态管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/admin/shop")
+public class AdminShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 管理端获取店铺经营状态
+     * @return 店铺状态
+     */
+    @GetMapping("/status")
+    public Result<Integer> getShopStatus() {
+        log.info("管理端获取店铺营业状态");
+        Integer status = Integer.valueOf((String) redisTemplate.opsForValue().get("shop_status"));
+        return Result.success(status);
+    }
+
+    /**
+     * 管理端设置店铺经营状态
+     * @param status 店铺状态
+     * @return 成功操作结果
+     */
+    @PutMapping("/{status}")
+    public Result<String> updateShopStatus(@PathVariable("status") String status) {
+        log.info("管理端更改店铺营业状态，修改后营业状态status={}", status);
+        redisTemplate.opsForValue().set("shop_status", status);
+        return Result.success();
+    }
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/UserShopController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 店铺状态管理
+ */
+@Slf4j
+@RestController
+@RequestMapping("/user/shop")
+public class UserShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 用户端获取店铺经营状态
+     * @return 店铺状态
+     */
+    @GetMapping("/status")
+    public Result<Integer> getShopStatus() {
+        log.info("用户端获取店铺状态");
+        Integer status = Integer.parseInt((String) redisTemplate.opsForValue().get("shop_status"));
+        return Result.success(status);
+    }
+}
